f4623328ae2ae56b4be6d33e89b482b699ba7eea
Prameela Pydi <npydi7234@gmail.com>
2022-12-10 20:13:08 -0800

    commit tree 3536e606dac28cb99123a37390afd67abf9b89a3

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(4, 5));
+
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(4, 5));
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(subtract(4, 5));
+
+function divide(a, b) { 
+    return a / b;
+}
+console.log(subtract(4, 5));
+
+function increment(n) {
+    return n + 1;
+}
+console.log(increment(9));
+
+function decrement(n) {
+    return n - 1;
+}
+console.log(increment(9));
+
+function makeInt(str) {
+    return parseInt(str, 10);
+}
+console.log(makeInt("2"));
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+console.log(makeInt("2.22"));
+  
